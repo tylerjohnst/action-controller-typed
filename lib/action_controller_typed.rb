1611be8aeb4c12ae1ca0005289997ec701caa00e
Tyler Johnston <tylerjohnst@gmail.com>
2025-02-04 17:39:13 -0500

    Hello world

---
+++ lib/action_controller_typed.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "action_controller_typed/version"
+
+module ActionControllerTyped
+  class Error < StandardError; end
+  # Your code goes here...
+end
